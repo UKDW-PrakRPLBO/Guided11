631c52565ae9652a69d31aff158da081ac8de38c
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2023-05-10 06:59:10 +0700

    Update Main.java

--- src/main/java/com/rplbo/fileexception/Main.java
+++ src/main/java/com/rplbo/fileexception/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
-        
+        //Input nama file
+        //Buat object MyFile dengan nama file tersebut, validasi! 
     }
 }
